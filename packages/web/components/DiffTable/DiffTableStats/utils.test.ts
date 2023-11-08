d7d59331dfba760b4ee1ac08defac7f0a1de52c8
Taylor Bantle <taylor@dolthub.com>
2023-11-08 13:09:14 -0800

    web: Fix test

--- packages/web/components/DiffTable/DiffTableStats/utils.test.ts
+++ packages/web/components/DiffTable/DiffTableStats/utils.test.ts
@@ -36,7 +36,7 @@ describe("test getDoltCommitDiffQuery for diff tables", () => {
         type: CommitDiffType.ThreeDot,
       }),
     ).toEqual(
-      `SELECT diff_type, \`from_id\`, \`to_id\`, from_commit, from_commit_date, to_commit, to_commit_date FROM \`dolt_commit_diff_${params.tableName}\` WHERE from_commit="${params.fromRefName}" AND to_commit=DOLT_MERGE_BASE("${params.toRefName}", "${params.fromRefName}")`,
+      `SELECT diff_type, \`from_id\`, \`to_id\`, from_commit, from_commit_date, to_commit, to_commit_date FROM \`dolt_commit_diff_${params.tableName}\` WHERE from_commit=DOLT_MERGE_BASE("${params.toRefName}", "${params.fromRefName}") AND to_commit=HASHOF("${params.fromRefName}")`,
     );
   });
 });
