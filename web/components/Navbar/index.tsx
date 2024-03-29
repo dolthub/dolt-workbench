2f3f1716b41efe3524f9c461a32dbb714890db76
Taylor Bantle <taylor@dolthub.com>
2024-03-29 09:58:01 -0700

    web: RC Navbar

--- web/components/Navbar/index.tsx
+++ web/components/Navbar/index.tsx
@@ -1,39 +1,47 @@
 import DocsLink from "@components/links/DocsLink";
 import Link from "@components/links/Link";
-import { ExternalLink } from "@dolthub/react-components";
+import { ExternalLink, Navbar } from "@dolthub/react-components";
 import { dockerHubRepo, workbenchGithubRepo } from "@lib/constants";
 import { FaDocker } from "@react-icons/all-files/fa/FaDocker";
 import { FaGithub } from "@react-icons/all-files/fa/FaGithub";
-import css from "./index.module.css";
 
-export default function Navbar() {
+export default function Nav() {
   return (
-    <div className={css.container}>
-      <div className={css.inner}>
-        <div className={css.left}>
-          <DocsLink className={css.link}>Documentation</DocsLink>
-          <Link className={css.link} href="/connections">
-            Connections
-          </Link>
-        </div>
+    <Navbar
+      logo={<Logo />}
+      leftLinks={<LeftLinks />}
+      rightLinks={<RightLinks />}
+      bgColor="bg-ld-darkerblue"
+    />
+  );
+}
 
-        <div>
-          <Link href="/" className={css.workbenchLogoLink}>
-            <img src="/images/dolt-workbench.png" alt="Dolt Workbench" />
-          </Link>
-        </div>
+function LeftLinks() {
+  return (
+    <>
+      <DocsLink>Documentation</DocsLink>
+      <Link href="/connections">Connections</Link>
+    </>
+  );
+}
 
-        <div className={css.right}>
-          <div className={css.hubLinks}>
-            <ExternalLink href={workbenchGithubRepo} className={css.link}>
-              <FaGithub /> GitHub
-            </ExternalLink>
-            <ExternalLink href={dockerHubRepo} className={css.link}>
-              <FaDocker /> Docker Hub
-            </ExternalLink>
-          </div>
-        </div>
-      </div>
-    </div>
+function RightLinks() {
+  return (
+    <>
+      <ExternalLink href={workbenchGithubRepo}>
+        <FaGithub /> GitHub
+      </ExternalLink>
+      <ExternalLink href={dockerHubRepo}>
+        <FaDocker /> Docker Hub
+      </ExternalLink>
+    </>
+  );
+}
+
+function Logo() {
+  return (
+    <Link href="/">
+      <img src="/images/dolt-workbench.png" alt="Dolt Workbench" />
+    </Link>
   );
 }
