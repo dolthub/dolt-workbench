46145467fbb5c0d64184702d2506e718ac1484f0
LiuLiu <liuliu.developer@gmail.com>
2024-09-06 12:01:49 -0700

    call hooks conditionally

--- web/contexts/serverConfig.tsx
+++ web/contexts/serverConfig.tsx
@@ -1,7 +1,8 @@
-// import { ErrorMsg, Loader } from "@dolthub/react-components";
+import { ErrorMsg, Loader } from "@dolthub/react-components";
 import { createContextWithDisplayName } from "@dolthub/react-contexts";
-// import { useEffectAsync } from "@dolthub/react-hooks";
+import { useEffectAsync } from "@dolthub/react-hooks";
 import { ReactNode, useContext, useState } from "react";
+import useSWR from "swr";
 
 type ServerConfig = {
   graphqlApiUrl: string | undefined;
@@ -19,51 +20,83 @@ type Props = {
   children: ReactNode;
 };
 
+type InnerProps = Props & {
+  children: ReactNode;
+  data: ServerConfigContextValue | undefined;
+  error: any;
+};
+
 // Custom hook to fetch the server config using IPC
-// function useServerConfigIPC(): {
-//   data: ServerConfigContextValue | null;
-//   error: any;
-// } {
-//   const [data, setData] = useState<ServerConfigContextValue | null>(null);
-//   const [error, setError] = useState<any>(null);
+function useServerConfigIPC(): {
+  data: ServerConfigContextValue | undefined;
+  error: any;
+} {
+  const [data, setData] = useState<ServerConfigContextValue | undefined>(
+    undefined,
+  );
+  const [error, setError] = useState<any>(undefined);
 
-//   useEffectAsync(async () => {
-//     const fetchConfig = async () => {
-//       try {
-//         const config = await window.ipc.invoke("api-config");
-//         setData(config);
-//       } catch (err) {
-//         setError(err);
-//       }
-//     };
+  useEffectAsync(async () => {
+    const fetchConfig = async () => {
+      try {
+        const config = await window.ipc.invoke("api-config");
+        setData(config);
+      } catch (err) {
+        setError(err);
+      }
+    };
 
-//     await fetchConfig();
-//   }, []);
+    await fetchConfig();
+  }, []);
 
-//   return { data, error };
-// }
+  return { data, error };
+}
+
+function ApiConfigProvider({ children }: Props): JSX.Element {
+  const { data, error } = useSWR<ServerConfigContextValue>("/api/config");
+  return (
+    <Provider data={data} error={error}>
+      {children}
+    </Provider>
+  );
+}
+
+function IPCConfigProvider({ children }: Props): JSX.Element {
+  const { data, error } = useServerConfigIPC();
+  return (
+    <Provider data={data} error={error}>
+      {children}
+    </Provider>
+  );
+}
 
 // ServerConfigProvider needs to wrap every page, and is only used in _app
 export function ServerConfigProvider({ children }: Props): JSX.Element {
-  // const { data, error } = useServerConfigIPC();
+  return process.env.NEXT_PUBLIC_FOR_ELECTRON === "true" ? (
+    <IPCConfigProvider>{children}</IPCConfigProvider>
+  ) : (
+    <ApiConfigProvider>{children}</ApiConfigProvider>
+  );
+}
 
-  // if (error) {
-  //   return (
-  //     <>
-  //       <ErrorMsg err={error} />
-  //       {children}
-  //     </>
-  //   );
-  // }
-  
-const data: ServerConfigContextValue = {
-  graphqlApiUrl:undefined,
-};
-  return  (
+function Provider({ children, data, error }: InnerProps): JSX.Element {
+  if (error) {
+    error.message = `Failed to fetch server config,data:${data},error:${error}, ${error.message}`;
+    return (
+      <>
+        <ErrorMsg err={error} />
+        {children}
+      </>
+    );
+  }
+
+  return data ? (
     <ServerConfigContext.Provider value={{ ...data }}>
       {children}
     </ServerConfigContext.Provider>
-  )  
+  ) : (
+    <Loader loaded={false} />
+  );
 }
 
 export function useServerConfig(): ServerConfigContextValue {
