98c295dd7a4d55efa48996351f84897316981d72
LiuLiu <liuliu.developer@gmail.com>
2024-09-16 14:53:17 -0700

    conditional config path

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: {
     "tailwindcss/nesting": {},
     tailwindcss: {
-      config: "./web/tailwind.config.ts",
+      config: process.env.NEXT_PUBLIC_FOR_ELECTRON === "true"?"./web/tailwind.config.ts":"./tailwind.config.ts",
     },
     "postcss-preset-env": {},
     cssnano: {},
