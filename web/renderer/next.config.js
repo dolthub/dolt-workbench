3b733a4acd85f029f20803b8ad890562a19dc175
LiuLiu <liuliu.developer@gmail.com>
2024-10-25 11:24:26 -0700

    dev:app,build:app

--- web/renderer/next.config.js
+++ web/renderer/next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  distDir: process.env.NODE_ENV === "production" ? "../app" : "./.next",
+  distDir:
+    process.env.NEXT_PUBLIC_FOR_ELECTRON === "true" &&
+    process.env.NODE_ENV === "production"
+      ? "../app"
+      : "../.next",
   output:
     process.env.NEXT_PUBLIC_FOR_ELECTRON === "true" &&
     process.env.NODE_ENV === "production"
