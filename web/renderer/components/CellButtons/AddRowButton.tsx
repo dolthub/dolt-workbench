408f15bd600f790d3f43ee184dfb34991cc4f407
LiuLiu <liuliu.developer@gmail.com>
2025-02-27 14:36:53 -0800

    prettier

--- web/renderer/components/CellButtons/AddRowButton.tsx
+++ web/renderer/components/CellButtons/AddRowButton.tsx
@@ -4,7 +4,10 @@ import { useDataTableContext } from "@contexts/dataTable";
 import useSqlBuilder from "@hooks/useSqlBuilder";
 import { isUneditableDoltSystemTable } from "@lib/doltSystemTables";
 import { toPKColsMapQueryCols } from "./utils";
-import { ColumnForDataTableFragment, RowForDataTableFragment } from "@gen/graphql-types";
+import {
+  ColumnForDataTableFragment,
+  RowForDataTableFragment,
+} from "@gen/graphql-types";
 import HideForNoWritesWrapper from "@components/util/HideForNoWritesWrapper";
 import { Button } from "@dolthub/react-components";
 import { ColumnValue } from "@hooks/useSqlBuilder/util";
@@ -15,34 +18,39 @@ type Props = {
   columns: ColumnForDataTableFragment[];
   refName?: string;
 };
- 
+
 export default function AddRowButton(props: Props) {
-    const { executeQuery, setEditorString } = useSqlEditorContext();
-    const { params, columns } = useDataTableContext();
-    const { tableName } = params;
-    const { insertIntoTable } = useSqlBuilder();
-  
-    if (!tableName || isUneditableDoltSystemTable(tableName)) return null;
-  
-    const onClick = async () => {
-      const query = insertIntoTable(
-        tableName,
-        columns?.map(c => c.name) ?? [],
-        columns?.map(c =>{return {type:c.type,value:c.isPrimaryKey?randomNum(0,10000):`" "`}}) ?? []
-      );
-      setEditorString(query);
-      await executeQuery({
-        ...params,
-        refName: props.refName ?? params.refName,
-        query,
-      });
-    };
-  
-    return (
-      <HideForNoWritesWrapper params={params}>
-        <Button.Link onClick={onClick} className={css.button}>
-          Add row
-        </Button.Link>
-      </HideForNoWritesWrapper>
+  const { executeQuery, setEditorString } = useSqlEditorContext();
+  const { params, columns } = useDataTableContext();
+  const { tableName } = params;
+  const { insertIntoTable } = useSqlBuilder();
+
+  if (!tableName || isUneditableDoltSystemTable(tableName)) return null;
+
+  const onClick = async () => {
+    const query = insertIntoTable(
+      tableName,
+      columns?.map(c => c.name) ?? [],
+      columns?.map(c => {
+        return {
+          type: c.type,
+          value: c.isPrimaryKey ? randomNum(0, 10000) : `" "`,
+        };
+      }) ?? [],
     );
+    setEditorString(query);
+    await executeQuery({
+      ...params,
+      refName: props.refName ?? params.refName,
+      query,
+    });
+  };
+
+  return (
+    <HideForNoWritesWrapper params={params}>
+      <Button.Link onClick={onClick} className={css.button}>
+        Add row
+      </Button.Link>
+    </HideForNoWritesWrapper>
+  );
 }
