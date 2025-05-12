807fe8b2a152423bb2d661f3bd40626853e0e3b5
LiuLiu <liuliu.developer@gmail.com>
2025-05-12 14:00:22 -0700

    connections list

--- workbench-cypress/cypress/e2e/publicPaths/render/connections/index.spec.ts
+++ workbench-cypress/cypress/e2e/publicPaths/render/connections/index.spec.ts
@@ -2,6 +2,7 @@ import {
   shouldFindAndContain,
   shouldFindButton,
 } from "@sharedTests/sharedFunctionsAndVariables";
+import { newExpectation, newShouldArgs } from "@utils/helpers";
 import { runTests } from "@utils/index";
 
 const pageName = "Homepage";
@@ -15,6 +16,11 @@ describe(pageName, () => {
       "Connect the workbench to any MySQL or PostgreSQL compatible database. ",
     ),
     shouldFindButton("add-connection-button"),
+    newExpectation(
+      "should find connections list with at least 1 connection",
+      "[data-cy=connections-list] > li",
+      newShouldArgs("be.visible.and.have.length.of.at.least", 1),
+    ),
     shouldFindAndContain(
       "connection-workbench-cypress-test",
       "workbench-cypress-test",
