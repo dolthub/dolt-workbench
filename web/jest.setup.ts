4cfc8857004fa83c8465959a75bdfb59faf21ade
Eric Richardson <erictaylorrichardson@gmail.com>
2026-02-02 13:35:02 -0800

    Revert "fix test issues with updated next version"
    This reverts commit 22151aebb48c6c6645e0dcb398c574db9c24e7bc.

--- web/jest.setup.ts
+++ web/jest.setup.ts
@@ -1,1 +1,9 @@
 import "@testing-library/jest-dom";
+import { setConfig } from "next/config";
+import config from "./renderer/next.config.js";
+
+// Make sure you can use "publicRuntimeConfig" within tests.
+setConfig({
+  ...config,
+  publicRuntimeConfig: { ...config.publicRuntimeConfig, useQueryMocks: false },
+});
--- web/renderer/next.config.js
+++ web/renderer/next.config.js
@@ -15,8 +15,6 @@ const nextConfig = {
     unoptimized:
       process.env.NEXT_PUBLIC_FOR_ELECTRON === "true" ? true : undefined,
   },
-  // Acknowledge Turbopack is default in Next.js 16; webpack config is still needed for preprocessor
-  turbopack: {},
   webpack: config => {
     // Add the webpack-preprocessor-loader so we can use getServerSideProps conditionally
     config.module.rules.push({
