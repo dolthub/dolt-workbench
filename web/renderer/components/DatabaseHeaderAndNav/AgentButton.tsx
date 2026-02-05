4194bf085fe350f29f5630f5567ee516d9389292
Eric Richardson <erictaylorrichardson@gmail.com>
2026-02-05 12:23:47 -0800

    disable agent mode for doltgres, add outline around agent button

--- web/renderer/components/DatabaseHeaderAndNav/AgentButton.tsx
+++ web/renderer/components/DatabaseHeaderAndNav/AgentButton.tsx
@@ -1,13 +1,15 @@
 import { useAgentContext } from "@contexts/agent";
+import useDatabaseDetails from "@hooks/useDatabaseDetails";
 import { RiRobot3Fill } from "react-icons/ri";
 import cx from "classnames";
 import css from "./index.module.css";
 
 export default function AgentButton() {
   const { togglePanel, isPanelOpen } = useAgentContext();
+  const { isDolt, isPostgres } = useDatabaseDetails();
 
-  // Only show in Electron environment
-  if (process.env.NEXT_PUBLIC_FOR_ELECTRON !== "true") {
+  // Only show in Electron environment, and not for doltgres databases
+  if (process.env.NEXT_PUBLIC_FOR_ELECTRON !== "true" || (isDolt && isPostgres)) {
     return null;
   }
 
