e10179cc027e7247a384d244717f1b72dc2f1423
Eric Richardson <erictaylorrichardson@gmail.com>
2026-08-06 12:44:48 -0700

    PR feedback

--- web/renderer/components/DataTable/index.tsx
+++ web/renderer/components/DataTable/index.tsx
@@ -73,12 +73,13 @@ function WithContext(props: TableProps) {
     workingDiffRowsToggled,
   } = useDataTableContext();
 
-  if (
-    (!workingDiffRowsToggled && loading && (!rows || !columns)) ||
-    (workingDiffRowsToggled &&
-      loadingWorkingDiff &&
-      (!workingDiffRows || !columns))
-  ) {
+  const loadingRows = !workingDiffRowsToggled && loading && (!rows || !columns);
+  const loadingWorkingDiffRows =
+    workingDiffRowsToggled &&
+    loadingWorkingDiff &&
+    (!workingDiffRows || !columns);
+
+  if (loadingRows || loadingWorkingDiffRows) {
     return <Loader loaded={false} />;
   }
 
